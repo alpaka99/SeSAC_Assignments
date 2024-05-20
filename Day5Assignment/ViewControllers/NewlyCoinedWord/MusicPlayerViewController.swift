fabc717cafe6680f42dcd5119734d949d32ca361
Lets <ruvan123@naver.com>
2024-05-21 01:58:53 +0900

    Feat: AutoLayout을이용한 음악 재생 화면 레이아웃 구현

--- Day5Assignment/ViewControllers/NewlyCoinedWord/MusicPlayerViewController.swift
+++ Day5Assignment/ViewControllers/NewlyCoinedWord/MusicPlayerViewController.swift
@@ -8,23 +8,69 @@
 import UIKit
 
 class MusicPlayerViewController: UIViewController {
-
+    @IBOutlet var titleImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var titleSubLabel: UILabel!
+    
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var likeView: UIButton!
+    @IBOutlet var followView: UIButton!
+    @IBOutlet var instagramIconView: UIButton!
+    
+    @IBOutlet var lyricLabel: UILabel!
+    @IBOutlet var playLoopButton: UIButton!
+    @IBOutlet var playShuffleButton: UIButton!
+    
+    lazy var middleButtons: [UIButton] = [
+        playLoopButton,
+        playShuffleButton
+    ]
+    
+    @IBOutlet var listButton: UIButton!
+    @IBOutlet var volumeButton: UIButton!
+    
+    lazy var bottomOuterButtons: [UIButton] = [
+        listButton,
+        volumeButton
+    ]
+    
+    @IBOutlet var previousButton: UIButton!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var nextButton: UIButton!
+    
+    lazy var bottomInnerButtons: [UIButton] = [
+        previousButton,
+        playButton,
+        nextButton
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemIndigo
+        // set title area
+        
+        // set album image area
+        
+        // set lyric label
+        
+        // set progress area
+        
+        // set bottom area
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+private enum MiddleButtonType: CaseIterable {
+    case playLoop
+    case playShuffle
+}
 
+private enum BottomOuterButtonType: CaseIterable {
+    case list
+    case volume
+}
+
+private enum BottomInnerButtonType: CaseIterable {
+    case previous
+    case play
+    case next
 }
