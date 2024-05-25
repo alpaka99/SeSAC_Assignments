91f1592916e44ed4f01b56f8ece716a54707403c
Lets <ruvan123@naver.com>
2024-05-26 00:25:10 +0900

    Feat: Magazine 프로젝트 시작

---
+++ MagazineProject/MagazineProject/MagazineTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MagazineTableViewController.swift
+//  MagazineProject
+//
+//  Created by user on 5/26/24.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
