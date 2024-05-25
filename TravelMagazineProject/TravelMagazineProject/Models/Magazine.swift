de1db68a0b49c6f2b24cc22cadaa2ade6ddb4ad8
Lets <ruvan123@naver.com>
2024-05-26 00:36:13 +0900

    Feat: Magazine 모델 생성하기

---
+++ TravelMagazineProject/TravelMagazineProject/Models/Magazine.swift
@@ -0,0 +1,15 @@
+//
+//  Magazine.swift
+//  TravelMagazineProject
+//
+//  Created by user on 5/26/24.
+//
+
+
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let photo_image: String
+    let date: String
+    let link: String
+}
