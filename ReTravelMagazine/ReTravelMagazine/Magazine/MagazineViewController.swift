a28ad39c3ba91dac9598f622683b6b3d4adea2c2
Lets <ruvan123@naver.com>
2024-06-01 21:10:22 +0900

    Feat: setup magazineViewController's tableview

---
+++ ReTravelMagazine/ReTravelMagazine/Magazine/MagazineViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MagazineViewController.swift
+//  ReTravelMagazine
+//
+//  Created by user on 6/1/24.
+//
+
+import UIKit
+
+final class MagazineViewController: UIViewController {
+    
+    let tableView: UITableView = UITableView()
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "SeSAC Magazine"
+        
+        layoutComponents()
+        configureComponents()
+    }
+    
+    private func layoutComponents() {
+        layoutTableView()
+    }
+    
+    private func configureComponents() {
+        configureTableView()
+    }
+    
+    private func layoutTableView() {
+        self.view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+        
+    }
+}
+
+extension MagazineViewController: UITableViewDelegate, UITableViewDataSource {
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = "Magazine"
+        
+        return cell
+    }
+}
+
+extension UITableViewCell {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
