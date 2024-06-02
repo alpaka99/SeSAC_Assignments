044ebab9e8212eaa6c1c53bf4885564bd26ccc0b
Lets <ruvan123@naver.com>
2024-06-02 16:02:54 +0900

    Feat: Travel tableview 레이아웃 구현

--- ReTravelMagazine/ReTravelMagazine/Travel/TravelViewController.swift
+++ ReTravelMagazine/ReTravelMagazine/Travel/TravelViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class TravelViewController: UIViewController {
+    
+    let tableView: UITableView = UITableView()
+    
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
@@ -17,6 +20,67 @@ final class TravelViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        layoutComponents()
+        
+        configureComponents()
+    }
+    
+    private func layoutComponents() {
+        layoutTableView()
+    }
+    
+    private func layoutTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    
+    private func configureComponents() {
+        configureNavigationItem()
+        configureTableView()
+    }
+    
+    private func configureNavigationItem() {
         navigationItem.title = "SeSAC Travel"
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+    }
+}
+
+
+extension TravelViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath) as UITableViewCell
+            
+            cell.textLabel?.text = "• even number cell"
+            cell.textLabel?.textColor = .systemOrange
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath) as UITableViewCell
+            
+            cell.textLabel?.text = "• odd number cell"
+            cell.textLabel?.textColor = .systemBlue
+            
+            return cell
+        }
     }
 }
