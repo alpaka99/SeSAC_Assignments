8e27d93a9d4efff04de8cc17da158206eaafd289
Lets <ruvan123@naver.com>
2024-06-04 03:19:53 +0900

    Feat(Unfinished): keyboard 높이만큼 textfield 올렸다 내리기

--- ReTravelMagazine/ReTravelMagazine/Views/Talk/TalkRoomViewController.swift
+++ ReTravelMagazine/ReTravelMagazine/Views/Talk/TalkRoomViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 final class TalkRoomViewController: UIViewController {
     
     private let tableView: UITableView = UITableView()
+    private let textFieldBackground: UIView = UIView()
+    private let textField: UITextField = UITextField()
+    private let submitButton: UIButton = UIButton()
     private var chats: [Chat] = []
     
     override func loadView() {
@@ -34,6 +37,9 @@ final class TalkRoomViewController: UIViewController {
     
     private func layoutComponents() {
         layoutTableView()
+        layoutTextFieldBackground()
+        layoutTextField()
+        layoutSubmitButton()
     }
     
     private func layoutTableView() {
@@ -45,12 +51,60 @@ final class TalkRoomViewController: UIViewController {
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+    }
+    
+    private func layoutTextFieldBackground() {
+        view.addSubview(textFieldBackground)
+        
+        textFieldBackground.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textFieldBackground.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 8),
+            textFieldBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textFieldBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textFieldBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(tabBarController?.tabBar.frame.height ?? 0) - 8),
+            textFieldBackground.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        
+        textFieldBackground.backgroundColor = .systemGray6
+        textFieldBackground.layer.cornerRadius = 8
+    }
+    
+    private func layoutTextField() {
+        textFieldBackground.addSubview(textField)
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: textFieldBackground.topAnchor, constant: 8),
+            textField.leadingAnchor.constraint(equalTo: textFieldBackground.leadingAnchor, constant: 8),
+            textField.bottomAnchor.constraint(equalTo: textFieldBackground.bottomAnchor, constant: -8),
+        ])
+        
+        textField.placeholder = "메세지를 입력하세요"
+    }
+    
+    private func layoutSubmitButton() {
+        textFieldBackground.addSubview(submitButton)
+        
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            submitButton.topAnchor.constraint(equalTo: textFieldBackground.topAnchor, constant: 8),
+            submitButton.leadingAnchor.constraint(equalTo: textField.trailingAnchor, constant: 8),
+            submitButton.trailingAnchor.constraint(equalTo: textFieldBackground.trailingAnchor, constant: -8),
+            submitButton.bottomAnchor.constraint(equalTo: textFieldBackground.bottomAnchor, constant: -8),
+            submitButton.widthAnchor.constraint(equalTo: submitButton.heightAnchor, multiplier: 1)
+        ])
+        
+        submitButton.setImage(UIImage(systemName: "arrowtriangle.forward"), for: .normal)
+        submitButton.tintColor = .systemGray4
     }
     
     private func configureComponents() {
         configureTableView()
+        configureKeyboard()
     }
     
     private func configureTableView() {
@@ -62,6 +116,38 @@ final class TalkRoomViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
         tableView.register(OpponentTalkCell.self, forCellReuseIdentifier: OpponentTalkCell.reuseIdentifier)
         tableView.register(UserTalkCell.self, forCellReuseIdentifier: UserTalkCell.reuseIdentifier)
+    }
+    
+    private func configureKeyboard() {
+        textField.addTarget(self, action: #selector(submitted), for: .editingDidEndOnExit)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc
+    func submitted(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @objc
+    func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            UIView.animate(withDuration: 0.1) { [weak self] in
+                // force unwrapping 수정
+                NSLayoutConstraint.activate([
+                    self!.textFieldBackground.bottomAnchor.constraint(equalTo: self!.view.bottomAnchor, constant: -(keyboardFrame.cgRectValue.height+8))
+                ])
+            }
+        }
+    }
+    
+    @objc
+    func keyboardDidHide(_ notification: Notification) {
+        print(#function)
+        
+//        textFieldBackground.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -8).isActive = true
+        layoutTextFieldBackground()
     }
     
     internal func configureData(_ data: [Chat]) {
