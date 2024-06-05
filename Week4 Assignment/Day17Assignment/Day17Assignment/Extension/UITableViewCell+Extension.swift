671bfd4c7d4be28b89afd29f6eb8c7f68a417cbe
Lets <ruvan123@naver.com>
2024-06-05 17:49:48 +0900

    Chore: 폴더링 작업 및 extension 분리

---
+++ Week4 Assignment/Day17Assignment/Day17Assignment/Extension/UITableViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Extension.swift
+//  Day17Assignment
+//
+//  Created by user on 6/5/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
--- Week4 Assignment/Day17Assignment/Day17Assignment/Views/MovieRanking/MovieRankingViewController.swift
+++ Week4 Assignment/Day17Assignment/Day17Assignment/Views/MovieRanking/MovieRankingViewController.swift
@@ -104,12 +104,6 @@ extension MovieRankingViewController: UITableViewDelegate, UITableViewDataSource
     
 }
 
-extension UITableViewCell {
-    static var identifier: String {
-        return String(describing: self)
-    }
-}
-
 
 //https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101
 
