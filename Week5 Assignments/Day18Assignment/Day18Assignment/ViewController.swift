812941a6891df9678d171b33d023a3605238843d
Lets <ruvan123@naver.com>
2024-06-10 18:05:45 +0900

    Feat: 프로젝트 storyboard 삭제 및 초기 세팅

---
+++ Week5 Assignments/Day18Assignment/Day18Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day18Assignment
+//
+//  Created by user on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemOrange
+    }
+
+
+}
+
