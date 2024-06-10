a1f5f5c31eaa75c7793b800e70bac4597e1c4f69
Lets <ruvan123@naver.com>
2024-06-10 18:12:48 +0900

    Feat: Create Reusable Protocol

---
+++ Week5 Assignments/Day18Assignment/Day18Assignment/Protocol/Reusable.swift
@@ -0,0 +1,17 @@
+//
+//  Reusable.swift
+//  Day18Assignment
+//
+//  Created by user on 6/10/24.
+//
+
+protocol Reusable {
+    
+}
+
+
+extension Reusable {
+    var identifier: String {
+        return String(describing: self)
+    }
+}
--- Week5 Assignments/Day18Assignment/Day18Assignment/SceneDelegate.swift
+++ Week5 Assignments/Day18Assignment/Day18Assignment/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         
-        window?.rootViewController = ViewController()
+        window?.rootViewController = TrendingViewController()
         window?.makeKeyAndVisible()
     }
 
