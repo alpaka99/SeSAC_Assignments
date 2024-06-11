2e6c378d905ed16a3da650236f0c2ba826814631
Lets <ruvan123@naver.com>
2024-06-11 18:26:37 +0900

    Feat: 검색화면 기본 레이아웃 구현

--- Week5 Assignments/Day19Assignment/Day19Assignment/SearchCollectionViewCell.swift
+++ Week5 Assignments/Day19Assignment/Day19Assignment/SearchCollectionViewCell.swift
@@ -5,4 +5,42 @@
 //  Created by user on 6/11/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class SearchCollectionViewCell: UICollectionViewCell {
+    let posterImage: UIImageView = UIImageView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+extension SearchCollectionViewCell: CodeBaseBuildable {
+    func configureHierarchy() {
+        self.addSubview(posterImage)
+    }
+    
+    func configureLayout() {
+        posterImage.snp.makeConstraints {
+            $0.edges.equalTo(self)
+        }
+    }
+    
+    func configureUI() {
+        posterImage.layer.cornerRadius = 8
+        posterImage.clipsToBounds = true
+        posterImage.contentMode = .scaleToFill
+    }
+    
+    func configureData<T>(_ data: T) {
+        
+    }
+    
+    
+}
--- Week5 Assignments/Day19Assignment/Day19Assignment/ViewController.swift
+++ Week5 Assignments/Day19Assignment/Day19Assignment/ViewController.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 
+import Alamofire
+import SnapKit
+
 class ViewController: UIViewController {
     let searchBar: UISearchBar = UISearchBar()
-    let collectionView: UIView = UIView()
+    lazy var collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: collectionViewLayout()
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
     }
 }
 
@@ -25,16 +34,61 @@ extension ViewController: CodeBaseBuildable {
     }
     
     func configureLayout() {
+        searchBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
         
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func configureUI() {
-        <#code#>
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .systemIndigo
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
     }
     
     func configureData<T>(_ data: T) {
-        <#code#>
+        print(#function)
+    }
+}
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.identifier, for: indexPath) as? SearchCollectionViewCell  else { return UICollectionViewCell() }
+        
+        cell.backgroundColor = .systemOrange
+        
+        return cell
+    }
     
+    func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        let width = (ScreenSize.width - 40) / 3
+        layout.itemSize = CGSize(width: width, height: width * 1.3)
+        
+        return layout
+    }
+}
+
+
+struct ScreenSize {
+    static let width: CGFloat = UIScreen.main.bounds.width
+    static let height: CGFloat = UIScreen.main.bounds.height
 }
