ad627939ab664e668bdf952fb94d160914f0365b
Lets <ruvan123@naver.com>
2024-06-19 23:44:08 +0900

    Feat: Implement RestaurantViewController

--- Week6 Assignments/day24 Assignment/day24 Assignment/Constants/Constants.swift
+++ Week6 Assignments/day24 Assignment/day24 Assignment/Constants/Constants.swift
@@ -5,7 +5,18 @@
 //  Created by user on 6/19/24.
 //
 
+import CoreLocation
+
 struct ImageName {
     static let restaurantTabImage = "fork.knife.circle.fill"
     static let weatherCheckTabImage = "cloud.circle.fill"
+    static let targetImage = "scope"
+}
+
+
+struct LocationCoordinate {
+    static let dummyLocation = CLLocationCoordinate2D(
+        latitude: 37.51780,
+        longitude: 126.88629
+    )
 }
--- Week6 Assignments/day24 Assignment/day24 Assignment/Views/Restaurant/RestaurantViewController.swift
+++ Week6 Assignments/day24 Assignment/day24 Assignment/Views/Restaurant/RestaurantViewController.swift
@@ -5,27 +5,92 @@
 //  Created by user on 6/19/24.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
+import SnapKit
+
 final class RestaurantViewController: UIViewController {
+    let mapView = MKMapView()
+    let userLocationButton = UIButton()
+        .image(systemName: ImageName.targetImage)
+        .tintColor(with: .white)
+        .backgroundColor(with: .systemBlue)
+        .cornerStyle(style: .capsule)
+    
+    let restaurants: [Restaurant] = RestaurantList().restaurantArray
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         configureHierarchy()
         configureLayout()
         configureUI()
+        
+        LocationStore.shared.checkLocationAvailabilty()
     }
     
     func configureHierarchy() {
-        
+        view.addSubview(mapView)
+        view.addSubview(userLocationButton)
     }
     
     func configureLayout() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
         
+        userLocationButton.snp.makeConstraints {
+            $0.leading.equalTo(mapView.snp.leading)
+                .offset(20)
+            $0.bottom.equalTo(mapView.snp.bottom)
+                .offset(-20)
+            $0.size.equalTo(44)
+        }
     }
     
     func configureUI() {
         view.backgroundColor = .white
+        
+        navigationItem.title = "식당 정보 검색하기"
+        
+        mapView.showsUserLocation = true
+        
+        userLocationButton.addTarget(
+            self,
+            action: #selector(userLocationButtonTapped),
+            for: .touchUpInside
+        )
+        
+        addRestaurantAnnotations(restaurants)
+    }
+    
+    @objc
+    func userLocationButtonTapped(_ sender: UIButton) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: LocationStore.shared.fetchCurrentLocation(),
+            latitudinalMeters: 400,
+            longitudinalMeters: 400
+        )
+        
+        mapView.region = coordinateRegion
+    }
+    
+    func addRestaurantAnnotations(_ restaurants: [Restaurant]) {
+        var restaurantAnnotations: [MKPointAnnotation] = []
+        
+        for restaurant in restaurants {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(
+                latitude: restaurant.latitude,
+                longitude: restaurant.longitude
+            )
+            annotation.title = restaurant.name
+            
+            restaurantAnnotations.append(annotation)
+        }
+        
+        mapView.addAnnotations(restaurantAnnotations)
     }
 }
