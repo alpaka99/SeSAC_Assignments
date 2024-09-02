34c44dcbde31877788b432adff87c061523d7364
Lets <ruvan123@naver.com>
2024-09-02 20:41:42 +0900

    Feat: LaunchScreen 구현

--- Week16/SwiftUIRecap2/SwiftUIRecap2/LaunchView.swift
+++ Week16/SwiftUIRecap2/SwiftUIRecap2/LaunchView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct LaunchView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image("launch")
+                    .padding()
+                
+                Spacer()
+                Image("launchImage")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Spacer()
+                NavigationLink {
+                    ProfileSettingView()
+                } label: {
+                    Text("시작하기")
+                        .bold()
+                        .font(.headline)
+                }
+                .padding()
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .background(.blue)
+                .clipShape(Capsule())
+                .padding()
+                
+            }
+            .navigationTitle("some navigationTitle")
         }
-        .padding()
     }
 }
 
---
+++ Week16/SwiftUIRecap2/SwiftUIRecap2/ProfileSettingView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileSettingView.swift
+//  SwiftUIRecap2
+//
+//  Created by user on 9/2/24.
+//
+
+import SwiftUI
+
+struct ProfileSettingView: View {
+    var body: some View {
+        Text("안녕 세상아")
+    }
+}
+
+
+#Preview {
+    ProfileSettingView()
+}
