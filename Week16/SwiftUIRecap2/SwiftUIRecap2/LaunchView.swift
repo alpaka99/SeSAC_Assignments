4f19f5a0a1ea79103e93f7922805c87412ca2aa7
Lets <ruvan123@naver.com>
2024-09-02 20:34:26 +0900

    Feat: SwiftUIRecap2 초기화

---
+++ Week16/SwiftUIRecap2/SwiftUIRecap2/LaunchView.swift
@@ -0,0 +1,24 @@
+//
+//  LaunchView.swift
+//  SwiftUIRecap2
+//
+//  Created by user on 9/2/24.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LaunchView()
+}
---
+++ Week16/SwiftUIRecap2/SwiftUIRecap2/SwiftUIRecap2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecap2App.swift
+//  SwiftUIRecap2
+//
+//  Created by user on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
