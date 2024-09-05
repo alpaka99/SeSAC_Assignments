5bcf7978547f48a136e62ef822adb2f68f517674
Lets <ruvan123@naver.com>
2024-09-06 07:16:12 +0900

    Feat: CoinView 다시 구현

--- Week16/SwiftUIRecap2/SwiftUIRecap2/CoinView.swift
+++ Week16/SwiftUIRecap2/SwiftUIRecap2/CoinView.swift
@@ -4,81 +4,106 @@
 //
 //  Created by user on 9/3/24.
 //
+import SwiftUI
+
 
 import SwiftUI
 
 struct CoinView: View {
-    @State private var searchText = ""
-    @State private var allMarkets: Markets = []
-    @State private var filteredMarkets: Markets = []
+    @State private var markets: Markets = []
+    @State private var banner = Banner()
     
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVStack {
-                    ForEach($filteredMarkets) { $market in
-                        NavigationLink {
-                            DetailView(market: $market)
-                        } label: {
-                            RowView(market: $market)
-                        }
-                    }
-                }
-            }
-            .background {
-                Color.black
-                    .ignoresSafeArea()
-            }
-            .task {
-                do {
-                    allMarkets = try await UpbitAPI.fetchMarket()
-                    filteredMarkets = allMarkets
-                } catch {
-                    print("error: \(error.localizedDescription)")
-                }
+                bannerView()
                 
-            }
-            .searchable(text: $searchText, prompt: "Search")
-            .onChange(of: searchText) { newValue in
-                print(newValue)
-                if newValue.isEmpty {
-                    print("Empty")
-                    filteredMarkets = allMarkets
-                }
-                
-                let filteredData = allMarkets.filter {
-                    return $0.koreanName.localizedStandardContains(searchText) || $0.englishName.localizedStandardContains(searchText) || $0.market.localizedStandardContains(searchText)
-                }
-                filteredMarkets = filteredData
-                
+                listView()
             }
             .refreshable {
-                do {
-                    print("refreshing")
-                    allMarkets = try await UpbitAPI.fetchMarket()
-                } catch {
-                    print("error: \(error.localizedDescription)")
-                }
+                markets.shuffle()
             }
-            .navigationTitle("SearchView")
+            .navigationTitle("CoinView")
         }
+        .task {
+            do {
+                let result = try await UpbitAPI.fetchMarket()
+                markets = result
+            } catch {
+                print("Market Fetch Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // onAppear는 기본적으로 동기 환경에서 실행되므로, 비동기 작업을 처리하려면 추가적인 래핑이 필요(e.g DispatchQueue.global.async)
+    // 반면에 task는 기본적으로 비동기 환경이기때문에 이런 래핑 처리가 필요 없음
+    
+    func listView() -> some View {
+        return LazyVStack {
+            ForEach($markets) { $market in
+                rowView(market: $market)
+            }
+        }
+    }
+    
+    func bannerView() -> some View {
+        return RoundedRectangle(cornerRadius: 12)
+            .fill(banner.color)
+            .frame(height: 150)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .overlay(alignment: .leading) {
+                Circle()
+                    .fill(.white.opacity(0.3))
+                    .scaleEffect(2)
+            }
+            
+            .overlay(alignment: .bottomLeading) {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text(markets.randomElement()?.koreanName ?? "")
+                        .font(.callout)
+                    Text(markets.randomElement()?.englishName ?? "")
+                        .font(.title)
+                        .bold()
+                }
+                .foregroundStyle(.white)
+                .padding(20)
+            }
     }
 }
 
-struct RowView: View {
-    let bitCoinImages = [
-        "bitcoinsign",
-        "bitcoinsign.circle",
-        "bitcoinsign.circle.fill",
-        "bitcoinsign.square",
-        "bitcoinsign.square.fill"
-    ]
+#Preview {
+    CoinView()
+}
+
+
+struct Banner: Hashable, Identifiable {
+    let id = UUID()
+    var total = Int.random(in: 1000...5000) * 150
+    let color = Color.random()
+    
+    var totalFormat: String {
+        return String(total.formatted()) + "원"
+    }
+}
+
+
+extension Color {
+    static func random() -> Color {
+        return Color.init(
+            red: Double.random(in: 0...1),
+            green: Double.random(in: 0...1),
+            blue: Double.random(in: 0...1)
+        )
+    }
+}
+
+struct rowView: View {
     @Binding var market: Market
     
     var body: some View {
-        HStack(alignment: .center) {
-            Image(systemName: bitCoinImages.randomElement() ?? "bitcoinsign")
-                .foregroundStyle(Color.random())
+        
+        HStack {
             VStack(alignment: .leading) {
                 HStack {
                     Text(market.koreanName)
@@ -90,23 +115,15 @@ struct RowView: View {
                 Text(market.englishName)
                     .font(.caption)
                     .foregroundStyle(.gray)
-                
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 8)
             
             Button {
                 market.like.toggle()
             } label: {
-                Image(systemName: "star")
-                    .padding(.init(top: 10, leading: 10, bottom: 10, trailing: 0))
+                Image(systemName: market.like ? "star.fill" : "star")
             }
         }
-        .foregroundStyle(.white)
-        .padding(.horizontal, 20)
-        .padding(.vertical, 8)
     }
 }
-
-#Preview {
-    CoinView()
-}
-
